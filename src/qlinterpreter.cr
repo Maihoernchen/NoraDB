b824abd8113f15894a469392277367e82eca7d7a
Maihoernchen <72254540+Maihoernchen@users.noreply.github.com>
2023-09-21 19:08:53 +0200

    fuck i inted, me fix l8er

--- src/qlinterpreter.cr
+++ src/qlinterpreter.cr
@@ -4,19 +4,18 @@
 # 2. Do the whole thing ourselves
 
 # import possible tokens
-require "json"
 
-SRC = File.open("../rec/tokens.json") do |file|
-    JSON.parse(file)
-end
+KEYS = ["SELECT","SELECT *","SELECT DISTINCT","SELECT INTO","SELECT TOP","CREATE","CREATE DATABASE","DROP","UPDATE","DELETE","ALTER TABLE","GRANT","REVOKE","SAVEPOINT","ROLLBACK","TRUNCATE"];
+SPEC = ["WHERE","AND","OR","BETWEEN","LIKE","IN","IS NULL","IS NOT NULL","FROM"];
+AGGR = ["COUNT","SUM","AVG","MIN","MAX","GROUP BY","HAVING","ORDER BY","DESC","OFFSET","FETCH"];
 
-# get tokens from json
-KEYS = SRC["keys"];
-SPEC = SRC["specifications"];
-AGGR = SRC["aggregates"];
+p! KEYS[5];
+p! SPEC[2];
+p! AGGR[3];
+
 
 # grab next token (faulty, fix next session)
-def nextToken(cmd)
+def nextToken(cmd,first)
     endpoint = cmd.index(" ")
     p! typeof(endpoint.as(Int32))
     token = cmd[0, endpoint.as(Int32)]
@@ -24,16 +23,30 @@ def nextToken(cmd)
     j = 0
     k = 0
     l = 0
-    while i < 2
-        while j < 15
+    nextt = ""
+    if first==false
+        while j < 16
             p! KEYS[j]
+            j = j+1
         end
         while k < 9
-
+            p! SPEC[k]
+            k = k+1
         end
         while l < 11
-
+            p! AGGR[l]
+            l = l+1
         end
+    else
+        while j < 16
+            p! KEYS[j]
+            j = j+1
+        end
+    end
+    if nextt.blank?
+        return false,token
+    else
+        return true,token
     end
 end
 
@@ -45,9 +58,9 @@ end
 # interpret given command
 def interpret(cmd)
     i = 0
-    nextToken = nextToken(cmd)
-    if !nextToken
-        puts "Error with QL-Syntax. Unexpected token #{nextToken}"
+    geht,token = nextToken(cmd,false)
+    if !geht
+        puts "Error with QL-Syntax. Unexpected token #{token}"
     end
 end
 
