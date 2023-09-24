1e0c54a52e821f73a3af6c90d5c8775e43d5df55
Maihoernchen <72254540+Maihoernchen@users.noreply.github.com>
2023-09-24 21:54:35 +0200

    enhanced qlinterpreter, added extensions folder

--- src/qlinterpreter.cr
+++ src/qlinterpreter.cr
@@ -3,7 +3,9 @@
 # 1. Use existing SQL Module to process SQL-Querys
 # 2. Do the whole thing ourselves
 
-# import possible tokens
+# import extensions
+
+require "../meth/*"
 
 KEYS = ["SELECT","SELECT *","SELECT DISTINCT","SELECT INTO","SELECT TOP","CREATE","CREATE DATABASE","DROP","UPDATE","DELETE","ALTER TABLE","GRANT","REVOKE","SAVEPOINT","ROLLBACK","TRUNCATE"];
 SPEC = ["WHERE","AND","OR","BETWEEN","LIKE","IN","IS NULL","IS NOT NULL","FROM"];
@@ -16,37 +18,44 @@ p! AGGR[3];
 
 # grab next token (faulty, fix next session)
 def nextToken(cmd,first)
-    endpoint = cmd.index(" ")
-    p! typeof(endpoint.as(Int32))
-    token = cmd[0, endpoint.as(Int32)]
+    # define counting indexes and empty nextt[oken]
     i = 0
     j = 0
     k = 0
     l = 0
     nextt = ""
+    # loop through 
     if first==false
         while j < 16
-            p! KEYS[j]
+            if cmd==KEYS[j]
+                nextt = KEYS[j]
+            end
             j = j+1
         end
         while k < 9
-            p! SPEC[k]
+            if cmd==SPEC[k]
+                nextt = SPEC[k]
+            end
             k = k+1
         end
         while l < 11
-            p! AGGR[l]
+            if cmd==AGGR[l]
+                nextt = AGGR[l]
+            end
             l = l+1
         end
     else
         while j < 16
-            p! KEYS[j]
+            if cmd==KEYS[j]
+                nextt = KEYS[j]
+            end
             j = j+1
         end
     end
     if nextt.blank?
-        return false,token
+        return false,cmd
     else
-        return true,token
+        return true,cmd
     end
 end
 
@@ -58,10 +67,17 @@ end
 # interpret given command
 def interpret(cmd)
     i = 0
-    geht,token = nextToken(cmd,false)
-    if !geht
-        puts "Error with QL-Syntax. Unexpected token #{token}"
+    cmd = cmd.split
+    while i < cmd.size
+        geht,token = nextToken(cmd[i],false)
+        if !geht
+            puts "Error with QL-Syntax. Unexpected token #{token}"
+        else 
+            next_token = token
+        end
+        p! next_token
+        i=i+1
     end
 end
 
-interpret("SELECT * FROM ente")
+interpret("SelEct * FROM ente")
